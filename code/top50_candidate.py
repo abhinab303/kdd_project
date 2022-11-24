52b71255f758cf4e9e047acc8bb94b8d693188dc
Abhinab <abhinabacharya303@gmail.com>
2022-11-24 10:43:29 -0500

    both sampling candidate

--- code/top50_candidate.py
+++ code/top50_candidate.py
@@ -16,7 +16,7 @@ from sklearn.metrics import precision_score, f1_score, recall_score
 from sklearn.model_selection import train_test_split
 
 l = 50
-sampling = "normal"
+sampling = "both"
 
 if l == 50:
     max_iter = [1000]
